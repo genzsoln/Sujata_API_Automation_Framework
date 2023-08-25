936247512a4e20ad911a868be5f99024c275e8fc
WSujata <sujata.waghmare@genzsoln.com>
2023-08-25 18:04:28 +0530

    Extent report and Log4j2 Code For API Automation Framework

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -9,21 +9,21 @@ import api.endpoints.UserEndPoints;
 import api.payload.User;
 import io.restassured.response.Response;
 import org.testng.asserts.*;
-//import org.apache.log4j.Logger;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import java.util.logging.Logger;
 
 public class UserTests 
 {
 	Faker faker;
 	User userPayload;
+	public Logger logger; // this variable we have to use in setupdata method
 	
 	@BeforeClass //this will execution before starting execution of all the methods
-	public void setupData()
+	public void setup()
 	{
 		faker=new Faker();
 		userPayload=new User(); //what ever data we create using faker same data we have to pass to POJO class
-		
-		public Logger logger;
 		
 		userPayload.setId(faker.idNumber().hashCode());   //generating id
 		userPayload.setUsername(faker.name().username());
@@ -37,32 +37,46 @@ public class UserTests
 		// then this userPayload data we have to pass to testRequest i.e post,put,delete,get
 	
 		//Log
+		logger=LogManager.getLogger(this.getClass()); //return the logger
+		
+		logger.debug("Debugging---------------------------------");
 	}
 	
 	//For Create User
 	@Test(priority=1)
 	public void testPostUser()
 	{
+		logger.info("************** Create User *************");
+		
 		Response response=UserEndPoints.createUser(userPayload);
 		response.then().log().all();
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
+	
+		logger.info("************** User Is Created *************");
+
 	}
 	
 	//Get User
 	@Test(priority=2)
 	public void testGetUserByName()
 	{
+		logger.info("************** Reading User Info *************");
+
 		Response response=UserEndPoints.readUser(this.userPayload.getUsername()); //extract username // we refer payload by refering this keyword
 		//this will gives user name of your payload
 		response.then().log().all();
 		Assert.assertEquals(response.getStatusCode(), 200);
+	
+		logger.info("************** User Info is displayed *************");
 	}
 	
 	//update data Using payload
 	@Test(priority=3)
 	public void testUpdateUserByName()
 	{
+		logger.info("************** Updating User *************");
+
 		//update user requires 2 para username & payload
 		userPayload.setFirstName(faker.name().firstName());
 		userPayload.setLastName(faker.name().lastName());
@@ -73,6 +87,8 @@ public class UserTests
 		response.then().log().body().statusCode(200); //HI assertion comes with restasuured
 		Assert.assertEquals(response.getStatusCode(), 200);
 	
+		logger.info("************** User is Updated *************");
+
 		//Checking data after update
 		Response responseAfterupdate=UserEndPoints.readUser(this.userPayload.getUsername()); //extract username
 		Assert.assertEquals(responseAfterupdate.getStatusCode(), 200);
@@ -82,8 +98,13 @@ public class UserTests
 	@Test(priority=4)
 	public void testDeleteUserByName()
 	{
+		logger.info("************** Deleting User *************");
+
 		Response response=UserEndPoints.deleteUser(this.userPayload.getUsername()); //extract username	
 		Assert.assertEquals(response.getStatusCode(), 200);
+	
+		logger.info("************** USer Deleted *************");
+
 	}
 }
 
--- src/test/java/api/utilities/XLUtility.java
+++ src/test/java/api/utilities/XLUtility.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -107,4 +109,48 @@ public class XLUtility
         fi.close();
         fo.close();
     }
+    
+    public void fillGreenColor(String sheetName, int rownum, int column) throws IOException
+
+        {
+
+            
+
+            fi= new FileInputStream(path);
+
+            workbook=new XSSFWorkbook(fi);
+
+            sheet=workbook.getSheet(sheetName);
+
+            
+
+            row=sheet.getRow(rownum);
+
+            cell=row.getCell(column);
+
+            
+
+            style=workbook.createCellStyle();
+
+            
+
+            style.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+            
+
+            cell.setCellStyle(style);
+
+            workbook.write(fo);
+
+            workbook.close();
+
+            fi.close();
+
+            fo.close();
+
+            
+
+        }
 }
